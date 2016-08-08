6b8f6e4000952a931bff45c58a38226b04365713
James Brooks <jbrooksuk@me.com>
2016-08-08 15:42:13 +0100

    Fix CreateTimedActionCommandTest

--- tests/Bus/Commands/TimedAction/CreateTimedActionCommandTest.php
+++ tests/Bus/Commands/TimedAction/CreateTimedActionCommandTest.php
@@ -9,36 +9,38 @@
  * file that was distributed with this source code.
  */
 
-namespace CachetHQ\Tests\Cachet\Bus\Commands\Subscriber;
+namespace CachetHQ\Tests\Cachet\Bus\Commands\TimedAction;
 
 use AltThree\TestBench\CommandTrait;
 use CachetHQ\Cachet\Bus\Commands\TimedAction\CreateTimedActionCommand;
 use CachetHQ\Cachet\Bus\Handlers\Commands\TimedAction\CreateTimedActionCommandHandler;
 use CachetHQ\Cachet\Models\TimedActionGroup;
 use CachetHQ\Tests\Cachet\AbstractTestCase;
+use Carbon\Carbon;
 
 /**
  * This is the create timed action command test class.
  *
  * @author James Brooks <james@alt-three.com>
  */
-class CreatedTimedActionCommandTest extends AbstractTestCase
+class CreateTimedActionCommandTest extends AbstractTestCase
 {
     use CommandTrait;
 
     protected function getObjectAndParams()
     {
         $params = [
-            'name'               => 'support@cachethq.io',
-            'group'              => new TimedActionGroup(),
-            'description'        => 'Foo bar',
-            'active'             => true,
-            'timezone'           => 'Europe/London',
-            'window_length'      => 3600,
-            'completion_latency' => 360,
+            'name'                  => 'support@cachethq.io',
+            'description'           => 'Foo bar',
+            'start_at'              => Carbon::now()->toDateTimeString(),
+            'active'                => true,
+            'timezone'              => 'Europe/London',
+            'window_length'         => 3600,
+            'completion_latency'    => 360,
+            'timed_action_group_id' => new TimedActionGroup(),
         ];
 
-        $object = new CreateTimedActionCommand($params['name'], $params['description'], $params['active'], $params['timezone'], $params['window_length'], $params['completion_latency'], $params['group']);
+        $object = new CreateTimedActionCommand($params['name'], $params['description'], $params['active'], $params['start_at'], $params['timezone'], $params['window_length'], $params['completion_latency'], $params['timed_action_group_id']);
 
         return compact('params', 'object');
     }
